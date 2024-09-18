a3a2c22bffcae53d0e3cad654e861cd9b630f2e6
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-09-18 14:48:12 -0500

    Tests are failing, but say "contents are identical" for all four. Not sure why???

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -84,7 +84,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -110,12 +110,17 @@ public class JobData {
         //We need to loop over allJobs to look for the search term (value).
         //So we need to loop over every key and look inside the values of each of the jobs.
 
+        //Case sensitivity, if we were doing equals I could use equalsIgnoreCase, but then it would rule out if the
+        //search term was mentioned in the value. So, I think I would have to just make the value and search term set
+        //to either Upper or Lowercase. I would think this needs to go here for All search & also for searching the
+        //columns as well.
+
         ArrayList<HashMap<String, String>> searchJobs = new ArrayList<>();
 
         for (HashMap<String, String> job : allJobs) {
 
                 for (String searchKey : job.keySet()) {
-                    if (job.get(searchKey).contains(value)) {
+                    if (job.get(searchKey).toLowerCase().contains(value.toLowerCase())) {
                         if (!searchJobs.contains(job)) {
                             searchJobs.add(job);
                         }
